0d5141a80830f09fa941c527c4139eb5d4dea02f
Grumpy Programmer <grumpy-programmer@users.noreply.github.com>
2022-09-06 11:32:08 +0200

    feat: create spring-actuator-build-info (#1)

---
+++ spring-actuator-build-info/src/test/java/io/grumpyprogrammer/example/spring/actuator/buildinfo/SpringActuatorBuildInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.grumpyprogrammer.example.spring.actuator.buildinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorBuildInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
