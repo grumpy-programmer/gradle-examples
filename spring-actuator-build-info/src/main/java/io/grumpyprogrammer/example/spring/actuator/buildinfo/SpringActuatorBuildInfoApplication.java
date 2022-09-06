c4df1881db4dcd2e52e444789778255475dac5e0
Grumpy Programmer <very.grumpy.programmer@gmail.com>
2022-09-06 11:28:41 +0200

    feat: create spring-actuator-build-info

---
+++ spring-actuator-build-info/src/main/java/io/grumpyprogrammer/example/spring/actuator/buildinfo/SpringActuatorBuildInfoApplication.java
@@ -0,0 +1,13 @@
+package io.grumpyprogrammer.example.spring.actuator.buildinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorBuildInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActuatorBuildInfoApplication.class, args);
+    }
+
+}
